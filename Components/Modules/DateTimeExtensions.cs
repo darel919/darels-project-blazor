e0fdd10248d9d53fea52412f61f409bf921955b6
Darrell C <darrell.cristanto@hotmail.com>
2025-02-20 10:23:07 +0700

    feat: add time ago

---
+++ Components/Modules/DateTimeExtensions.cs
@@ -0,0 +1,36 @@
+using System;
+
+public static class DateTimeExtensions
+{
+    public static string GetTimeAgo(this DateTime dateTime)
+    {
+        var timeSpan = DateTime.Now - dateTime;
+
+        if (timeSpan.TotalDays > 365)
+        {
+            int years = (int)(timeSpan.TotalDays / 365);
+            return years == 1 ? "1 year ago" : $"{years} years ago";
+        }
+        else if (timeSpan.TotalDays > 30)
+        {
+            int months = (int)(timeSpan.TotalDays / 30);
+            return months == 1 ? "1 month ago" : $"{months} months ago";
+        }
+        else if (timeSpan.TotalDays > 1)
+        {
+            return $"{(int)timeSpan.TotalDays} days ago";
+        }
+        else if (timeSpan.TotalHours > 1)
+        {
+            return $"{(int)timeSpan.TotalHours} hours ago";
+        }
+        else if (timeSpan.TotalMinutes > 1)
+        {
+            return $"{(int)timeSpan.TotalMinutes} minutes ago";
+        }
+        else
+        {
+            return "just now";
+        }
+    }
+}
